beea039071b534db2e4b55869936ed61cdc172d5
lucascoraca <lucascoracasilva@gmail.com>
2020-09-10 09:51:20 -0300

    added mariadb database

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 cd store
-python3 /store/manage.py migrate && python3 /store/manage.py runserver 0.0.0.0:80
+python3 /store/manage.py migrate && python3 /store/manage.py runserver 0.0.0.0:80 > logs.txt
--- store/settings.py
+++ store/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'store',
+        'USER': 'root',
+        'PASSWORD': 'pdc_dev',
+        'HOST': 'mariadb',
+        'PORT': '3306',
     }
 }
 
